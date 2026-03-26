283a3dcb53f0136f147afdf74cbb46115ef839b4
Nikolay Kumanov <nikolaykumanov@nikolays-MacBook-Pro.local>
2026-03-26 21:00:18 +0200

    fix build

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,10 +1,9 @@
 import styles from "./Image.module.scss";
 
 type ImageProps = {
-  id: string;
   url: string;
 };
-const Image = ({ id, url }: ImageProps) => {
+const Image = ({ url }: ImageProps) => {
   return (
     <div className={styles.imageContainer}>
       <img src={url} alt={"Image still loading"} className={styles.image} />
